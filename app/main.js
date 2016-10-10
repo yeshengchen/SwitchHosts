4295c9d0ee595e683bbcdae40b6b461846774907
yes <864840673@qq.com>
2016-10-10 11:00:22 +0800

    启动时检查是否已经启动过实例，禁止同时启动多个实例

--- app/main.js
+++ app/main.js
@@ -70,6 +70,18 @@ function createWindow() {
     });
 }
 
+var shouldQuit = app.makeSingleInstance(function(commandLine, workingDirectory) {
+    // Someone tried to run a second instance, we should focus our window.
+    if (mainWindow) {
+        if (mainWindow.isMinimized()) mainWindow.restore();
+        mainWindow.focus();
+    }
+    return true;
+});
+if (shouldQuit) {
+    app.quit();
+    return;
+}
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
